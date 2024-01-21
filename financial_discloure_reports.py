d7db2b1222c70fbd1993457c631150b8feba146f
alexktong <alexktong.92@gmail.com>
2024-01-21 09:12:18 +0000

    update code

--- financial_discloure_reports.py
+++ financial_discloure_reports.py
@@ -2,8 +2,14 @@
 
 import os
 import pandas as pd
+from datetime import datetime
+import configparser
 
-list_of_last_names = ['foxx', 'green']
+
+def create_directory(directory_path):
+
+    if not os.path.exists(directory_path):
+        os.makedirs(directory_path)
 
 
 def download_fd_zip(url_source, input_directory):
@@ -11,6 +17,9 @@ def download_fd_zip(url_source, input_directory):
     # Downloads the source file into specified directory
     os.system(f'wget -N -P {input_directory} {url_source}')
     
+
+def unzips_fd_zip(input_directory, file_name):
+
     # Unzips the source file into input directory
     os.system(f'unzip -o -d {input_directory} {input_directory}{file_name}')
 
@@ -25,34 +34,68 @@ def get_fd_text_file(file_name):
 
 
 def read_fd_text_file(input_directory, fd_file_txt):
-
-    fd_file_txt = get_fd_text_file()
     
     df = pd.read_csv(os.path.join(input_directory, fd_file_txt), sep='\t')
 
+    df['FilingDate'] = pd.to_datetime(df['FilingDate'], format='%m/%d/%Y').dt.date
+
+    df['URL'] = 'https://disclosures-clerk.house.gov/public_disc/ptr-pdfs/2024/' + df['DocID'].astype(str) + '.pdf'
+
     return df
 
 
-def get_fd_by_last_name(df, list_of_last_names):
+def get_fd_by_last_name(df, list_of_last_names, filing_date_lag_days):
     
+    # Filter by last name
     df = df[df['Last'].str.lower().isin(list_of_last_names)]
 
+    # Filter by filing date
+    date_today = datetime.today().date()
+
+    filing_date_since = date_today - pd.Timedelta(days=filing_date_lag_days)
+
+    df = df[df['FilingDate'] >= filing_date_since]
+
     return df
+
 
 def main():
 
+    # Load config file
+    config_file = 'config.ini'
+
+    config_obj = configparser.ConfigParser()
+    config_obj.read(config_file)
+    
     # Link to source file
-    url_source = 'https://disclosures-clerk.house.gov/public_disc/financial-pdfs/2024FD.zip'
+    url_source = config_obj.get('input', 'url_source')
 
     # Directory to save the source file
-    input_directory = 'input/'
+    input_directory = config_obj.get('input', 'directory')
+    create_directory(input_directory)
+
+    # Directory to save the output file
+    output_directory = config_obj.get('output', 'directory')
+    create_directory(output_directory)
+
+    # Name of output file
+    output_file = config_obj.get('output', 'file_name')
+
+    # Reports of individuals to keep
+    list_of_last_names = config_obj.get('input', 'list_of_last_names')
+    list_of_last_names = list_of_last_names.split(', ')
+
+    # Reports within specified time period
+    filing_date_lag_days = config_obj.getint('input', 'filing_date_lag_days')
 
     # Downloads the zipped FD file
     download_fd_zip(url_source, input_directory)
 
-
-    # Source file name
+    # Zipped file name
     file_name = os.path.basename(url_source)
+
+    # Unzips zipped FD file
+    unzips_fd_zip(input_directory, file_name)
 
     # Retrieve the txt file
     file_name_txt = get_fd_text_file(file_name)
@@ -61,7 +104,11 @@ def main():
     df = read_fd_text_file(input_directory, file_name_txt)
 
     # Return FDs of select individuals by last name
-    df = get_fd_by_last_name(df, list_of_last_names)
+    df = get_fd_by_last_name(df, list_of_last_names, filing_date_lag_days)
+
+    # Save to a CSV file
+    df.to_csv(os.path.join(output_directory, output_file), index=False)
+
 
 if __name__ == '__main__':
     main()
