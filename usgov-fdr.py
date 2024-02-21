4ce477042c7c37ec071caf066a48eb48bc5220ed
alexktong <alexktong.92@gmail.com>
2024-02-21 12:53:50 +0000

    update

--- usgov-fdr.py
+++ usgov-fdr.py
@@ -7,36 +7,25 @@ from datetime import datetime
 import configparser
 
 
+# Load config file
+def load_config(config_file):
+    config_file = config_file
+
+    config_obj = configparser.ConfigParser()
+    config_obj.read(config_file)
+
+    return config_obj
+
+
 def create_directory(directory_path):
 
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
 
-def download_fd_zip(url_source, input_directory):
-
-    # Downloads the source file into specified directory
-    os.system(f'wget -N -P {input_directory} {url_source}')
+def read_fd_text_file(directory_to_fd_file_txt):
     
-
-def unzips_fd_zip(input_directory, file_name):
-
-    # Unzips the source file into input directory
-    os.system(f'unzip -o -d {input_directory} {os.path.join(input_directory, file_name)}')
-
-
-def get_fd_text_file(file_name):
-
-    file_name_without_extension = os.path.splitext(file_name)[0]
-
-    file_name_txt = f'{file_name_without_extension}.txt'
-
-    return file_name_txt
-
-
-def read_fd_text_file(input_directory, fd_file_txt):
-    
-    df = pd.read_csv(os.path.join(input_directory, fd_file_txt), sep='\t')
+    df = pd.read_csv(directory_to_fd_file_txt, sep='\t')
 
     df['FilingDate'] = pd.to_datetime(df['FilingDate'], format='%m/%d/%Y').dt.date
 
@@ -45,21 +34,8 @@ def read_fd_text_file(input_directory, fd_file_txt):
     return df
 
 
-def get_fd_by_full_name(df, list_of_full_names, filing_date_lag_days):
-    
-    # Construct full name
-    df['FullName'] = df['First'].str.lower() + ' ' + df['Last'].str.lower()
+def get_fd_for_past_days(df, filing_date_lag_days):
 
-    # Filter by full name
-    list_of_full_names_to_keep = []
-    for full_name in list_of_full_names:
-        full_name_to_keep = difflib.get_close_matches(full_name, df['FullName'], n=1)
-
-        if len(full_name_to_keep) > 0:
-            list_of_full_names_to_keep.append(full_name_to_keep[0])
-
-    df = df[df['FullName'].isin(list_of_full_names_to_keep)]
-    
     # Filter by filing date
     date_today = datetime.today().date()
 
@@ -74,52 +50,51 @@ def get_fd_by_full_name(df, list_of_full_names, filing_date_lag_days):
     return df
 
 
+def get_fd_by_full_name(df, list_of_full_names):
+    
+    # Construct full name
+    df['FullName'] = df['First'].str.lower() + ' ' + df['Last'].str.lower()
+
+    # Filter by full name
+    list_of_full_names_to_keep = []
+    for full_name in list_of_full_names:
+        full_name_to_keep = difflib.get_close_matches(full_name, df['FullName'], n=1)
+
+        if len(full_name_to_keep) > 0:
+            list_of_full_names_to_keep.append(full_name_to_keep[0])
+
+    df = df[df['FullName'].isin(list_of_full_names_to_keep)]
+
+    return df
+
+
 def main():
 
     # Load config file
-    config_file = 'config.ini'
-
-    config_obj = configparser.ConfigParser()
-    config_obj.read(config_file)
+    config_file = os.path.join(os.path.dirname(__file__), 'config.ini')
+    config_obj = load_config(config_file)
     
-    # Link to source file
-    url_source = config_obj.get('input', 'url_source')
-
-    # Directory to save the source file
-    input_directory = config_obj.get('input', 'directory')
-    create_directory(input_directory)
-
     # Directory to save the output file
     output_directory = config_obj.get('output', 'directory')
     create_directory(output_directory)
 
     # Name of output file
     output_file = config_obj.get('output', 'file_name')
+    
+    # Read the txt file as a dataframe
+    fd_text_file = config_obj.get('input', 'directory_to_fd_text_file')
+    df = read_fd_text_file(fd_text_file)
 
-    # Reports of individuals to keep
+    # Return FDs within specified time period
+    filing_date_lag_days = config_obj.getint('input', 'filing_date_lag_days')
+    
+    df = get_fd_for_past_days(df, filing_date_lag_days)
+
+    # Return FDs of select individuals by last name
     list_of_full_names = config_obj.get('input', 'list_of_full_names')
     list_of_full_names = list_of_full_names.split(', ')
 
-    # Reports within specified time period
-    filing_date_lag_days = config_obj.getint('input', 'filing_date_lag_days')
-
-    # Downloads the zipped FD file
-    download_fd_zip(url_source, input_directory)
-
-    # Zipped file name
-    file_name = os.path.basename(url_source)
-
-    # Unzips zipped FD file
-    unzips_fd_zip(input_directory, file_name)
-
-    # Retrieve the txt file
-    file_name_txt = get_fd_text_file(file_name)
-
-    # Read the txt file as a dataframe
-    df = read_fd_text_file(input_directory, file_name_txt)
-
-    # Return FDs of select individuals by last name
-    df = get_fd_by_full_name(df, list_of_full_names, filing_date_lag_days)
+    df = get_fd_by_full_name(df, list_of_full_names)
 
     # Save to a CSV file
     df = df[['Prefix', 'FullName', 'FilingDate', 'URL']]
